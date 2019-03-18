8688d022f8749d8c6bc19a1eef17c62033ccf0fe
DibbleM1999 <47530229+DibbleM1999@users.noreply.github.com>
2019-03-18 09:21:24 -0400

    New Wizard Class

---
+++ PlayerClasses/Wizard.py
@@ -0,0 +1,16 @@
+from Player import Player
+
+
+class Wizard(Player):
+    """
+    Wizard class, specializes in ranged attacks.
+    """
+
+    def __init__(self, start_pos, img="images/enemy1", stats=[1, 1, 10, 15]):
+        super().__init__(start_pos, img)
+
+        self.stats["MELEE"] = stats[0]
+        self.stats["RANGE"] = stats[1]
+        self.stats["MAGIC"] = stats[2]
+        self.stats["CUR_HP"] = stats[3]
+        self.stats["MAX_HP"] = stats[3]
